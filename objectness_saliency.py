3049779fd4c39dce631ba5a0cc097bbc575d19a9
ivanred6 <ivanfpanton@gmail.com>
2020-11-14 16:01:15 +0000

    Update objectness_saliency.py
    now to test...

--- objectness_saliency.py
+++ objectness_saliency.py
@@ -20,3 +20,25 @@ ap.add_argument("-a", "--here", required=False, help="Ignores SaveTo and uses CW
 image = cv2.imread(args["image"])
 
 di = args["diff"]
+
+# initialize OpenCV's objectness saliency detector and set the path
+# to the input model files
+saliency = cv2.saliency.ObjectnessBING_create()
+saliency.setTrainingPath(args["model"])
+# compute the bounding box predictions used to indicate saliency
+(success, saliencyMap) = saliency.computeSaliency(image)
+numDetections = saliencyMap.shape[0]
+
+# loop over the detections
+for i in range(0, min(numDetections, args["max_detections"])):
+	# extract the bounding box coordinates
+	(startX, startY, endX, endY) = saliencyMap[i].flatten()
+	
+	# randomly generate a color for the object and draw it on the image
+	output = image.copy()
+	color = np.random.randint(0, 255, size=(3,))
+	color = [int(c) for c in color]
+	cv2.rectangle(output, (startX, startY), (endX, endY), color, 2)
+	# show the output image
+	cv2.imshow("Image", output)
+	cv2.waitKey(0)
