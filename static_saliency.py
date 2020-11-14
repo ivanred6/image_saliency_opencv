ed215491afbb2f66f307fe54a4c5922b1c0cbc5c
ivanred6 <ivanfpanton@gmail.com>
2020-11-14 14:48:42 +0000

    Update static_saliency.py
    Added Binary Threshold Map

--- static_saliency.py
+++ static_saliency.py
@@ -16,4 +16,18 @@ saliencyMap = (saliencyMap * 255).astype("uint8")
 cv2.imshow("Image", img)
 cv2.imshow("Output", saliencyMap)
 cv2.waitKey(0)
-test = input("hello")
+
+# Initialise the more fine-grained saliency detector and compute the saliencyMap
+saliency = cv2.saliency.StaticSaliencyFineGrained_create()
+(success, saliencyMap) = saliency.computeSaliency(img)
+
+# If we want a *binary* map to use for contour processing,
+# computing convex hulls, extract bounding boxes, etc... we can 
+# additionally threshold the saliency map.
+threshMap = cv2.threshold(saliencyMap.astype("uint8"), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+# Show Images
+cv2.imshow("Image", img)
+cv2.imshow("Output", saliencyMap)
+cv2.imshow("Thresholds", threshMap)
+cv2.waitKey(0)
