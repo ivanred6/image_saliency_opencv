beff33f6ecc3b72500d9a9caaa78fc432d26a4f5
ivanred6 <ivanfpanton@gmail.com>
2020-11-14 15:02:47 +0000

    Update static_saliency.py
    Working for Threshold Saliency, Load / Save Images etc

--- static_saliency.py
+++ static_saliency.py
@@ -4,18 +4,21 @@ import cv2
 # We first build the argument parser, then parse args...
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", required=True, help="Path to input image")
+parser.add_argument("-o", "--save_to", required=True, help="Path to save image excluding the saliencyType")
+parser.add_argument("-d", "--diff", required=True, help="Differentiator")
 args = vars(parser.parse_args())
 
 # Load our input image
 img = cv2.imread(args["image"])
-
+di = args["diff"]
 # Initialise OpenCV's static saliency SPECTRAL RESIDUAL DETECTOR and compute saliency map
 saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
 (success, saliencyMap) = saliency.computeSaliency(img)
 saliencyMap = (saliencyMap * 255).astype("uint8")
-cv2.imshow("Image", img)
-cv2.imshow("Output", saliencyMap)
-cv2.waitKey(0)
+#cv2.imshow("Image", img)
+#cv2.imshow("Output", saliencyMap)
+cv2.imwrite(args["save_to"] + "/{}_lowfi_image_saliency.png".format(di), saliencyMap)
+#cv2.waitKey(0)
 
 # Initialise the more fine-grained saliency detector and compute the saliencyMap
 saliency = cv2.saliency.StaticSaliencyFineGrained_create()
@@ -30,4 +33,10 @@ threshMap = cv2.threshold(saliencyMap.astype("uint8"), 0, 255, cv2.THRESH_BINARY
 cv2.imshow("Image", img)
 cv2.imshow("Output", saliencyMap)
 cv2.imshow("Thresholds", threshMap)
+impath = args["save_to"] + "/{}_image_original.png".format(di)
+salpath = args["save_to"] + "/{}_image_saliency.png".format(di)
+threshpath = args["save_to"] + "/{}_image_thresh.png".format(di)
+cv2.imwrite(impath, img)
+cv2.imwrite(salpath, saliencyMap)
+cv2.imwrite(threshpath, threshMap)
 cv2.waitKey(0)
